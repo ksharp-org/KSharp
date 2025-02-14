988bc5be8440141a8eaaf3350f6fc496d0e83b5c
mario <mar1lusk1@proton.me>
2025-02-14 20:04:13 +0100

    Add types, rename things, etc.

---
+++ runtime/compiler/def/README.md
@@ -0,0 +1,18 @@
+﻿<div align="center">
+	<h1>The K# Compiler</h1>
+</div>
+
+This is the **default** compiler for K#. It is written in C++.
+
+## See more
+
+- [Repository](https://github.com/ksharp-org/KSharp)
+- [Documentation](https://docs.ksharp.org)
+
+## License
+
+K# is **Free and Open Source** that is **private** and **made by the community**.
+
+It is licensed under the **GNU General Public License v3.0**.
+
+See [LICENSE](https://github.com/ksharp-org/KSharp/blob/master/LICENSE) for more information.
---
+++ runtime/tools/types/lookup_hash.h
@@ -0,0 +1,84 @@
+﻿// Copyright (c) 2020 Mario Rosell
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+template <typename T>
+/**
+ * @brief LookupHash is a lightweight solution for hash maps in C++ similar to unordered_map.
+ * @author Mario Rosell
+ * @file lookup_hash.h
+ */
+struct LookupHash
+{
+    // Pairs
+    std::vector<std::pair<std::string, T>> pairs;
+
+    // Add a new key-value pair
+    void add(const std::string& key, const T& value)
+    {
+        pairs.emplace_back(key, value);
+    }
+
+    // Get value by key
+    T get(const std::string& key) const
+    {
+        for (const auto& pair : pairs)
+        {
+            if (pair.first == key)
+            {
+                return pair.second;
+            }
+        }
+        return T();  // Return default-constructed T if key not found
+    }
+
+    // Check if a key exists
+    bool has(const std::string& key) const
+    {
+        for (const auto& pair : pairs)
+        {
+            if (pair.first == key)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Remove a key-value pair
+    void remove(const std::string& key)
+    {
+        for (auto it = pairs.begin(); it != pairs.end(); ++it)
+        {
+            if (it->first == key)
+            {
+                pairs.erase(it);
+                return;
+            }
+        }
+    }
+
+    // Get number of elements
+    size_t size() const
+    {
+        return pairs.size();
+    }
+};
+
+/* Example usage:
+ *
+ * LookupHash<int> my_hash;
+ * my_hash.add("key1", 1);
+ * my_hash.add("key2", 2);
+ * my_hash.add("key3", 3);
+ *
+ * int value = my_hash.get("key1");
+ *
+ * if (my_hash.has("key2"))
+ * {
+ *     std::cout << "key2 exists" << std::endl;
+ * }
+*/
